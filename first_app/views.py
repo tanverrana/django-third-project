198229b2ce2d9fe7032fa113992b6fab2efd901c
pro <pro@prodeMacBook-Pro-2.local>
2023-07-31 11:41:47 +0600

    if else and for loop

--- first_app/views.py
+++ first_app/views.py
@@ -4,4 +4,20 @@ from django.shortcuts import render
 
 
 def contact(request):
-    return render(request, './first_app/index.html', context={'author': 'phitron'})
+    return render(request, './first_app/index.html', {'author': 'phitron', 'age': 19, 'marks': 90, 'courses': [
+        {
+            'id': 1,
+            'course': 'C',
+            'teacher': 'Arifin'
+        },
+        {
+            'id': 2,
+            'course': 'C++',
+            'teacher': 'Rokey'
+        },
+        {
+            'id': 3,
+            'course': 'Database',
+            'teacher': 'Iftekhar'
+        },
+    ]})
